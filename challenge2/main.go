1a93e5a0be3e6af19a60294bfc88bf379f71d420
Doychin Atanasov <ironsmile@gmail.com>
2013-10-21 00:18:53 +0300

    initialize the turns map only once

--- challenge2/main.go
+++ challenge2/main.go
@@ -5,38 +5,38 @@ type turns map[bool]string
 type DragonFractal struct {
 	iteration int
 	last      string
+	rotations map[string]turns
 }
 
 func (dragon *DragonFractal) Next() string {
 	var n = dragon.iteration
 
 	if n == 0 {
+		dragon.rotations = make(map[string]turns)
+
+		dragon.rotations["up"] = make(turns)
+		dragon.rotations["up"][true] = "left"
+		dragon.rotations["up"][false] = "right"
+
+		dragon.rotations["left"] = make(turns)
+		dragon.rotations["left"][true] = "down"
+		dragon.rotations["left"][false] = "up"
+
+		dragon.rotations["down"] = make(turns)
+		dragon.rotations["down"][true] = "right"
+		dragon.rotations["down"][false] = "left"
+
+		dragon.rotations["right"] = make(turns)
+		dragon.rotations["right"][true] = "up"
+		dragon.rotations["right"][false] = "down"
+
 		dragon.iteration += 1
 		dragon.last = "up"
 		return dragon.last
 	}
 
 	var turn_left bool = !((((n & -(n)) << 1) & n) != 0)
-
-	rotations := make(map[string]turns)
-
-	rotations["up"] = make(turns)
-	rotations["up"][true] = "left"
-	rotations["up"][false] = "right"
-
-	rotations["left"] = make(turns)
-	rotations["left"][true] = "down"
-	rotations["left"][false] = "up"
-
-	rotations["down"] = make(turns)
-	rotations["down"][true] = "right"
-	rotations["down"][false] = "left"
-
-	rotations["right"] = make(turns)
-	rotations["right"][true] = "up"
-	rotations["right"][false] = "down"
-
 	dragon.iteration += 1
-	dragon.last = rotations[dragon.last][turn_left]
+	dragon.last = dragon.rotations[dragon.last][turn_left]
 	return dragon.last
 }
